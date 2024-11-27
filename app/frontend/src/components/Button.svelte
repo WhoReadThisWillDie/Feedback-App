f63983fa10fb0abb9324743f2741f5ec53581e6d
Simona Waxmann <simona.waxmann@icloud.com>
2024-11-27 21:11:18 +0100

    Improve Tailwind setup, create UI template

---
+++ app/frontend/src/components/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let className = "";
+</script>
+
+<button
+  class="px-4 py-1 rounded-lg gradient-before gradient-border {className}"
+  on:click
+>
+  <slot />
+</button>
+
+<style>
+  button:hover {
+    background: linear-gradient(
+      to right,
+      #fd61e3 0%,
+      #8c62ec 50%,
+      #2b63f3 75%,
+      #31e1f7 100%
+    );
+  }
+</style>
---
+++ app/frontend/src/components/RecordButton.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Icon, Play, Pause } from "svelte-hero-icons";
+
+  import Button from "./Button.svelte";
+
+  let isRecording = false;
+
+  const toggleRecording = () => {
+    isRecording = !isRecording;
+    if (isRecording) {
+      // Start recording...
+    } else {
+      // Stop recording...
+    }
+  };
+</script>
+
+<div class="flex justify-center">
+  <Button
+    className="!rounded-full !p-0 w-16 h-16 flex items-center justify-center"
+    on:click={toggleRecording}
+  >
+    <Icon src={!isRecording ? Play : Pause} solid class="text-white size-10" />
+  </Button>
+</div>
