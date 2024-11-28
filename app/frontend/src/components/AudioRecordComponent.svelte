3713e93cce51023de0fc72e1d5a259c9e482c1b8
julie <522200@student.saxion.nl>
2024-11-28 14:18:44 +0100

    Audio recording functionality code is added and AudioRecordComponent.svelte

---
+++ app/frontend/src/components/AudioRecordComponent.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Button from "./Button.svelte";
+    import RecordButton from "./RecordButton.svelte";
+</script>
+
+
+<h2 class="font-medium">Transcript feedback</h2>
+<textarea
+        placeholder="Type here..."
+        class="w-full p-2 bg-gray-800 rounded-lg resize-none min-h-32"
+/>
+<RecordButton/>
+<div class="flex justify-center">
+    <Button>Submit</Button>
+</div>
