5d1cee448fbab13e6cfa73190830f4f965f12a44
DanilDuckk <danylo.kurbatov@gmail.com>
2024-12-01 17:18:13 +0100

    Added general endpoints and routes

---
+++ app/backend/src/controllers/assignment-controller.js
@@ -0,0 +1,11 @@
+export function getAllAssignments(req, res){}
+
+export function getAssignmentById(req, res){}
+
+export function addAssignment(req, res){}
+
+export function editAssignmentById(req, res){}
+
+export function deleteAssignmentById(req, res){}
+
+export function getAllFeedbacksForAssignment(req, res){}
--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
-const app = express()
+import bodyParser from 'body-parser';
+import userRouter from './routes/users.js';
+import assignmentRouter from './routes/assignments.js';
+import feedbackRouter from './routes/feedbacks.js';
+
+const app = express();
 const port = 3000
 
-app.use("/example", exampleRouter);
+app.use(bodyParser.json());
+app.use("/users", userRouter);
+app.use("/assignments", assignmentRouter);
+app.use("/feedback", feedbackRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
