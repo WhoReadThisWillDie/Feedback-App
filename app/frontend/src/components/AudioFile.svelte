ff023ad89034120e55658990a21e14dc172aea67
DanilDuckk <danylo.kurbatov@gmail.com>
2024-12-19 12:51:44 +0100

    animation added

--- app/frontend/src/components/AudioFile.svelte
+++ app/frontend/src/components/AudioFile.svelte
@@ -49,7 +49,7 @@
     }
 </script>
 
-<div class="mt-2 ml-5 w-[400px]">
+<div class="mt-2 ml-5 w-[400px] scale-in-center">
     <audio bind:this={audioElement} src={url} on:timeupdate={onTimeUpdate} on:loadedmetadata={onLoadedMetadata}></audio>
     <div class="flex items-center gap-2 p-4 rounded-full gradient-before gradient-border h-12 text-textColor">
         <Button className="!rounded-full !p-0 w-9 h-9 flex items-center justify-center" on:click={togglePlay}>
